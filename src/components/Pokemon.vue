95a2a3ea18ac2020c5c6d82d06c7d064ba899fe8
goncalveshigino <goncalveshigono20@gmail.com>
2021-03-11 16:02:47 +0100

    Pegando mais informacoes dos pokemons

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,7 +6,24 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+   created: function(){
+      axios.get(this.url).then( res => {
+          this.pokemon.type = res.data.types[0].type.name;
+          this.pokemon.front = res.data.sprites.front_default;
+          this.pokemon.back = res.data.sprites.back_default;
+          console.log(this.pokemon)
+      })
+   },
+   data(){
+      return {
+          pokemon: {
+
+          }
+      }
+   },
    props: {
        num: Number,
        name: String,
